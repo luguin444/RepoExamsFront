128ab2f8e301542d46feb82ca0fd036ac379ddae
luguin444 <gustavo.ganimi@engenharia.ufjf.br>
2020-12-18 12:56:58 -0300

    feat: Page of One professor Data

---
+++ src/pages/ProfessorPage/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const WrapperList = styled.div`
+    width: 100%;
+    margin: 0 auto;
+    display: flex;
+    flex-wrap: wrap;
+
+    div{
+        margin: 0 1rem;
+        min-width: 12rem;
+    }
+`
+
+export const Title = styled.h3`
+    margin-top: 2rem;
+    font-size: 1.8rem;
+    color: #fff;
+    text-align: center;
+
+`
